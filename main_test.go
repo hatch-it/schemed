80fc0edb76cbbf57a18cfa808560e7a7f0dcf7d0
Sam Balana <sambalana247@gmail.com>
2017-08-05 13:36:06 -0700

    Clean up the test file

--- main_test.go
+++ main_test.go
@@ -1,10 +1,7 @@
 package main_test
 
 import (
-	"os"
-	"log"
 	"testing"
-
 	"github.com/puradox/schemed"
 )
 
@@ -12,40 +9,4 @@ var app main.App
 
 func TestMain(m *testing.M) {
 	app = main.App{}
-	app.Initialize(
-		os.Getenv("TEST_DB_USERNAME"),
-		os.Getenv("TEST_DB_PASSWORD"),
-		os.Getenv("TEST_DB_NAME"),
-	)
-
-	ensureTableExists()
-
-	code := m.Run()
-
-	clearTable()
-
-	os.Exit(code)
-}
-
-func ensureTableExists() {
-	if _, err := app.DB.Exec(tableCreationQuery); err != nil {
-		log.Fatal(err)
-	}
-}
-
-const tableCreationQuery = `
-	-- User Table
-	CREATE TABLE IF NOT EXISTS users(
-		id         UUID         PRIMARY KEY     DEFAULT gen_random_uuid(),
-		created_at TIMESTAMP    WITH TIME ZONE  DEFAULT now(),
-		updated_at TIMESTAMP    WITH TIME ZONE  DEFAULT now(),
-		deleted_at TIMESTAMP		WITH TIME ZONE,
-
-		email      VARCHAR(254) NOT NULL,
-		password   VARCHAR(74)  NOT NULL
-	);
-`
-
-func clearTable() {
-	app.DB.Exec("DELETE FROM users")
 }

ef8533c266ae6098fcb0ea3076fba29a8fa1197c
Sam Balana <sambalana247@gmail.com>
2017-08-10 01:12:35 -0700

    Add initial support for filtering

--- model.go
+++ model.go
@@ -2,12 +2,22 @@ package main
 
 import (
 	"time"
+	"gopkg.in/mgo.v2/bson"
 )
 
 // Model represents a base database type
 type Model struct {
-  ID        uint        `json:"id" form:"id" bson:"_id,omitempty"`
-  CreatedOn time.Time
-  UpdatedOn time.Time
-  DeletedOn *time.Time
+  ID        bson.ObjectId `json:"id" form:"id" bson:"_id,omitempty"`
+  CreatedOn time.Time     `json:"createdOn" form:"createdOn"`
+  UpdatedOn time.Time     `json:"updatedOn" form:"updatedOn"`
+  DeletedOn *time.Time    `json:"deletedOn" form:"deletedOn"`
+}
+
+
+// ModelFilters contains all the possible filters for a Model
+type ModelFilters struct {
+  ID        *bson.ObjectId  `json:"id,omitempty" form:"id,omitempty" bson:"_id,omitempty"`
+  CreatedOn *time.Time      `json:"createdOn,omitempty" form:"createdOn,omitempty"`
+  UpdatedOn *time.Time      `json:"updatedOn,omitempty" form:"updatedOn,omitempty"`
+  DeletedOn *time.Time      `json:"deletedOn,omitempty" form:"deletedOn,omitempty"`
 }
